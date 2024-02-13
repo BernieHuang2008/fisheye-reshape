04fb66907ca113c47072ad648eac6c6fba8326d1
Nathan Chen <guangyu.chen40730-biph@basischina.com>
2024-02-13 20:00:57 +0800

    completed the other two quarter circles

--- reshape.py
+++ reshape.py
@@ -3,47 +3,54 @@ from math import sqrt
 import numpy as np
 from tqdm import tqdm
 
-# Load the image
-image = cv2.imread("input.jpg")
+image = cv2.imread("i.jpg")
 
-# reshape to 1000x1000
+# Reshape to 1000x1000
 image = cv2.resize(image, (1000, 1000))
 
 M = (500, 500)
 step = 1
 
-pixels = []
+pixels_upper = []
+pixels_lower = []
 
-for r in tqdm(range(0, 1000, step)):
+for r in tqdm(range(0, 500, step)):
     pxs1 = []
     pxs2 = []
-    for x in range(M[0] - r, M[0] + r):
+    pxs3 = []
+    pxs4 = []
+    for x in range(M[0], M[0] + r):
         # calculate y: (x-500)^2 + (y-500)^2 = r^2
         y1 = sqrt(r**2 - (x - M[0])**2) + M[1]
-        y2 = -sqrt(r**2 - (x - M[0])**2) + M[1]
+        y2 = M[1] - sqrt(r**2 - (x - M[0])**2) 
 
-        # on the circle
         x = int(min(x, 999))
         y1 = int(min(y1, 999))
-        y2 = int(min(y2, 999))
+        y2 = int(max(y2, 0)) 
 
         pxs1.append(image[y1, x])
-        pxs2.append(image[y2, x])
+        pxs2.append(image[y1, 999 - x])
+        pxs3.append(image[y2, x])
+        pxs4.append(image[y2, 999 - x])
     
-    pxs = pxs1 + pxs2[::-1]
-    pixels.append(pxs)
+    pixels_lower.append(pxs1 + pxs2[::-1])
+    pixels_upper.append(pxs3 + pxs4[::-1])
 
-W = 1000
+W = 1100
+H = 1000
 
-# construct image
-# construct image
-output_image = np.zeros((1000, 1000, 3), dtype=np.uint8)
+output_image = np.zeros((H, W, 3), dtype=np.uint8)
 
-for i in tqdm(range(1, len(pixels))):
-    pxs = pixels[i]
-    origin = np.array(pxs)
-    scaled = cv2.resize(np.array([pxs]), (W, 1))
-    output_image[i] = scaled[0]
+for i, pxs in enumerate(pixels_upper):
+    if pxs:
+        scaled = cv2.resize(np.array([pxs]), (W, 1), interpolation=cv2.INTER_LINEAR)
+        output_image[i] = scaled[0]
+
+middle_index = H // 2
+for i, pxs in enumerate(pixels_lower):
+    if pxs:
+        scaled = cv2.resize(np.array([pxs]), (W, 1), interpolation=cv2.INTER_LINEAR)
+        output_image[middle_index + i] = scaled[0]
 
 cv2.imshow("Output Image", output_image)
 cv2.waitKey(0)
